6384384a833920979cb7864ef5ca2455b7c57fbe
Anjila <anjilashrestha@gmail.com>
2025-07-04 17:23:48 +0545

    added text number of words and character

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 export default function TextForm(props) {
     const handleUpClick = () =>{
-        console.log("UpperCase was clicked!" + text);
+      //  console.log("UpperCase was clicked!" + text);
         let newText = text.toUpperCase();
         setText(newText)
     }
@@ -14,12 +14,18 @@ export default function TextForm(props) {
 //    text = 'new text'; //wrong way to change the state
 //    setText = ('new text'); //correct way to change the state
   return (
-    <div>
+    <>
+    <div className="container" my-2>
         <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea className="form-control"  value ={text} onChange={handleOnChange} id="myBox" rows="8" ></textarea>
       </div>
       <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
+    <div className="container my-3">
+      <h1>Your text summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      </div>
+    </>
   ); 
 }
