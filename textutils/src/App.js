f52e0ca6d568ec9644557a994b902168696733e4
Anjila <anjilashrestha@gmail.com>
2025-07-08 11:54:19 +0545

    modified code to make proper dark mode enable

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
+
 function App() {
+ const [mode, setMode] = useState('light');// dark mode enable xa ki nai
+
+ const toggleMode = () =>{
+  if(mode === 'light'){
+  setMode ('dark');
+  document.body.style.backgroundColor = 'grey';
+ } else {
+  setMode ('light');
+   document.body.style.backgroundColor = 'white';
+ }
+}
   return (
     <>
-<Navbar title = "TextUtils" aboutText = "About TextUtils"/>
-<div className="container" my-3>
-  <TextForm heading = "Enter the text to analyze"/>
-</div>
-
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode} />
+      <div className="container my-3">
+        <TextForm heading = "Enter the text to analyze" mode = {mode}/>
+        {/* <About /> */}
+      </div>
     </>
   );
 }
